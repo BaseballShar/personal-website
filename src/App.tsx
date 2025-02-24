599751c25bc0902c6bd86826e1c698f0289935ad
Baseball Chan <baseball@papabo.hk>
2025-02-24 17:44:15 +0800

    Created brief intro and contact list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { MdEmail } from "react-icons/md";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./App.css";
+
+const Introduction = () => {
+  return (
+    <div>
+      <h1>Baseball Chan</h1>
+      <p>Software Enginner</p>
+      <p>
+        I command computers to make my wish come true! (99% of the time I cry in
+        debugging)
+      </p>
+    </div>
+  );
+};
+
+const ContactList = () => {
+  return (
+    <div className="contact-list">
+      <a href="https://github.com/BaseballShar">
+        <FaGithub />
+      </a>
+      <a href="https://www.linkedin.com/in/baseball-chan-98b00923b/">
+        <FaLinkedin />
+      </a>
+      <a href="mailto:workingcth@gmail.com">
+        <MdEmail />
+      </a>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <>
-      <div>The beginning of Baseball's Website</div>
+      <Introduction />
+      <ContactList />
     </>
   );
 };
