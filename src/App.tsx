8ec9ea10357430145835e68a40b87144a0d39dce
Baseball Chan <baseball@papabo.hk>
2025-02-25 15:08:55 +0800

    Initial concept of Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { MdEmail } from "react-icons/md";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./App.css";
+import weatherPanel from "./assets/weather-panel.png";
+
+const colors = {
+  alphaBlue: "#38a3a5",
+  betaBlue: "#22577a",
+  alphaGreen: "#c7f9cc",
+  betaGreen: "#80ed99",
+  gammaGreen: "#57cc99",
+};
 
 const Introduction = () => {
   return (
@@ -31,11 +40,44 @@ const ContactList = () => {
   );
 };
 
+const Project = () => {
+  const content = {
+    title: "Weather Information System",
+    subtitle: "React, Next.js",
+    img: weatherPanel,
+  };
+  return (
+    <div
+      className="project-cell"
+      style={{ backgroundColor: colors.gammaGreen }}
+    >
+      <img className="project-img" src={weatherPanel} />
+      <p className="project-title">{content.title}</p>
+      <p className="project-subtitle">{content.subtitle}</p>
+    </div>
+  );
+};
+
+const ProjectList = () => {
+  return (
+    <div>
+      <h1 className="title">Projects</h1>
+      <div className="project-grid">
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <>
       <Introduction />
       <ContactList />
+      <ProjectList />
     </>
   );
 };
