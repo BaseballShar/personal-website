d1096f7c70a62aeaafa6d4e10117b5a5c4b25a88
Baseball Shar <workingcth@gmail.com>
2025-03-04 21:15:56 +0800

    Populated projects list with content

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,26 +1,80 @@
-import weatherPanel from "../../assets/weather-panel.png";
-import { colors } from "../../style";
 import "./Project.css";
+import moment from "moment";
+import { colors } from "../../style";
 
-const projectsData = [
+import bigTwoCover from "../../assets/big-two.png";
+import fypCover from "../../assets/fyp.png";
+import rpgCover from "../../assets/rpg.png";
+import shellCover from "../../assets/shell.png";
+import timetableCover from "../../assets/timetable.jpg";
+import weatherCover from "../../assets/weather.png";
+
+interface Project {
+  title: string;
+  subtitle: string;
+  dateStart: moment.Moment;
+  dateEnd: moment.Moment;
+  img: string;
+}
+
+const projectsData: [Project] = [
   {
     title: "Weather Information System",
     subtitle: "React, Next.js",
-    img: weatherPanel,
+    dateStart: moment("20240601"),
+    dateEnd: moment("20240601"),
+    img: weatherCover,
+  },
+  {
+    title: "Final Year Project",
+    subtitle: "Cross platform financial manager",
+    dateStart: moment("20230901"),
+    dateEnd: moment("20240401"),
+    img: fypCover,
+  },
+  {
+    title: "Timetable Management System",
+    subtitle: "Python, MySQL",
+    dateStart: moment("20221001"),
+    dateEnd: moment("20221101"),
+    img: timetableCover,
+  },
+  {
+    title: "Linux Shell",
+    subtitle: "C",
+    dateStart: moment("20221001"),
+    dateEnd: moment("20221001"),
+    img: shellCover,
+  },
+  {
+    title: "BigTwo Game",
+    subtitle: "Java",
+    dateStart: moment("20211001"),
+    dateEnd: moment("20211201"),
+    img: bigTwoCover,
+  },
+  {
+    title: "Text based RPG Game",
+    subtitle: "C++",
+    dateStart: moment("20210301"),
+    dateEnd: moment("20210401"),
+    img: rpgCover,
   },
 ];
 
-const Project = () => {
-  const content = {
-    title: "Weather Information System",
-    subtitle: "React, Next.js",
-    img: weatherPanel,
-  };
+const ProjectCell = ({ content }: { content: Project }) => {
   return (
     <div className="project-cell" style={{ backgroundColor: colors.secondary }}>
-      <img className="project-img" src={weatherPanel} />
+      <img className="project-img" src={content.img} />
       <p className="project-title">{content.title}</p>
       <p className="project-subtitle">{content.subtitle}</p>
+      <p className="project-subtitle">
+        {content.dateStart.isSame(content.dateEnd)
+          ? content.dateStart.format("MMM YYYY")
+          : content.dateStart.format("MMM YYYY") +
+            " - " +
+            content.dateEnd.format("MMM YYYY")}
+      </p>
     </div>
   );
 };
@@ -30,10 +84,9 @@ export const ProjectList = () => {
     <div>
       <h1 className="title">Projects</h1>
       <div className="project-grid">
-        <Project />
-        <Project />
-        <Project />
-        <Project />
+        {projectsData.map((item) => (
+          <ProjectCell content={item} />
+        ))}
       </div>
     </div>
   );
