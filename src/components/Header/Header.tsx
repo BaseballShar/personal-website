19739eaf6669dc597a8bde03548155cf7e6a4659
Baseball Shar <workingcth@gmail.com>
2025-03-19 16:42:44 +0800

    Created navigation header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+import { IoIosArrowBack } from "react-icons/io";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router";
+
+const Header = ({ title }) => {
+  return (
+    <div className="header-row">
+      <Link to="/personal-website">
+        <IoIosArrowBack className="header-icon" />
+      </Link>
+      <h1>{title || "Baseball's Place"}</h1>
+      <div className="header-right">
+        <a href="https://github.com/BaseballShar">
+          <FaGithub className="header-icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/baseball-chan-98b00923b/">
+          <FaLinkedin className="header-icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
