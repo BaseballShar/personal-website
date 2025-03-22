8175e1fb29047b0b9c63d1e385cec2788d2ee402
Baseball Shar <workingcth@gmail.com>
2025-03-22 18:36:39 +0800

    Implemented mobile friendly header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,27 +4,61 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { Link } from "react-router";
 
-const Header = ({ hasBack, title }: { hasBack?: boolean; title: string }) => {
+// Displays right justified buttons of contact
+const ContactBtns = () => {
   return (
-    <div className="header-row">
-      {hasBack && (
-        <Link to="/">
-          <IoIosArrowBack className="header-icon" />
-        </Link>
-      )}
-      <h1>{title || "Baseball's Place"}</h1>
-      <div className="header-right">
-        <a href="https://github.com/BaseballShar">
-          <FaGithub className="header-icon" />
-        </a>
-        <a href="https://www.linkedin.com/in/baseball-chan-98b00923b/">
-          <FaLinkedin className="header-icon" />
-        </a>
-        <a href="mailto:workingcth@gmail.com">
-          <MdEmail className="header-icon" />
-        </a>
-      </div>
+    <div className="btn-group">
+      <a href="https://github.com/BaseballShar">
+        <FaGithub className="header-icon" />
+      </a>
+      <a href="https://www.linkedin.com/in/baseball-chan-98b00923b/">
+        <FaLinkedin className="header-icon" />
+      </a>
+      <a href="mailto:workingcth@gmail.com">
+        <MdEmail className="header-icon" />
+      </a>
     </div>
+  );
+};
+
+const Header = ({ hasBack, title }: { hasBack?: boolean; title: string }) => {
+  const HeaderMobile = () => {
+    return (
+      <header className="mobile">
+        <h1 className="align-center">{title || "Baseball's Place"}</h1>
+        {hasBack ? (
+          <div className="header-row">
+            <Link to="/">
+              <IoIosArrowBack className="header-icon" />
+            </Link>
+            <ContactBtns />
+          </div>
+        ) : (
+          <ContactBtns />
+        )}
+      </header>
+    );
+  };
+
+  const HeaderDesktop = () => {
+    return (
+      <header className="desktop header-row">
+        {hasBack && (
+          <Link to="/">
+            <IoIosArrowBack className="header-icon" />
+          </Link>
+        )}
+        <h1>{title || "Baseball's Place"}</h1>
+        <ContactBtns />
+      </header>
+    );
+  };
+
+  return (
+    <>
+      <HeaderMobile />
+      <HeaderDesktop />
+    </>
   );
 };
 
